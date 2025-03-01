06e95b3bfec4cf294c2520218649b77de28f9022
crm105 <crm105@gwu.edu>
2025-02-28 21:05:58 -0500

    save predictions to pg

--- forecasting/forecasting.py
+++ forecasting/forecasting.py
@@ -1,22 +1,26 @@
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import pickle
 import yaml
 
-from sklearn.linear_model import LinearRegression
-from sqlalchemy import create_engine
-
 import sys
 sys.path.append('..')
 
 from utils.utils import pg_engine
+from sklearn.linear_model import LinearRegression
 
+
+#ToDo: Offload Engine Creation to Util, Draw from environmental variables 
 with open("../db_config.yml", "r") as file:
     db_config = yaml.safe_load(file)
 
 engine = pg_engine(db_config)
 
-query = "SELECT * FROM crm_mart.prediction_frame"
+# Let's only consider the week ahead 
+
+query = f"""SELECT * FROM crm_mart.prediction_frame
+            WHERE gamedate < CURRENT_DATE + 7"""
 prediction_frame = pd.read_sql(query, con = engine )
 
 #ToDo: Use config or other source to specify features to ensure 
@@ -26,10 +30,22 @@ X = prediction_frame[features]
 with open('../modeling/saved_models/model.pkl','rb') as f:
     model = pickle.load(f)
 
-prediction_frame['home_win_pred'] = model.predict(X)
+predicted_prob = model.predict(X)
+prediction_frame['home_win_prob'] = predicted_prob
+prediction_frame['home_win_predicted'] = np.rint(predicted_prob)
 
 prediction_frame['pirates_win_prob'] = np.where(prediction_frame['home_id'] == 134,
-                                                 prediction_frame['home_win_pred'],
-                                                 1 - prediction_frame['home_win_pred'])
+                                                 prediction_frame['home_win_prob'],
+                                                 1 - prediction_frame['home_win_prob'])
 
-#ToDo: Determine what to output and where to store output
+prediction_frame['pirates_win_predicted'] = np.where(prediction_frame['home_id'] == 134,
+                                                 prediction_frame['home_win_predicted'],
+                                                 1 - prediction_frame['home_win_predicted'])
+
+prediction_frame['forecast_date'] = datetime.today().strftime("%Y-%m-%d")
+prediction_frame = prediction_frame[['gamepk', 'gamedate', 'forecast_date',
+                                      'pirates_win_prob', 'pirates_win_predicted']]
+
+#Save prediction into PG
+prediction_frame.to_sql(name = "completed_predictions", schema = "crm_mart", con = engine,
+                        if_exists = "append", index=False)
